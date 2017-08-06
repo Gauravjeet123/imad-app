cef3e3c46b3dfdf8a878640dbcfe191a0da8e612
Gauravjeet Singh <30665291+Gauravjeet123@users.noreply.github.com>
2017-08-06 16:26:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,20 @@ element.innerHTML ='New value';
 // Move the image
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
+
+
+
 img.onclick = function(){
   
   // changing the CSS from Java Script
-  img.style.marginLeft = '100px';
+  var interval = setInterval(moveLeft,100);
+ // img.style.marginLeft = '100px';
+  
  };
