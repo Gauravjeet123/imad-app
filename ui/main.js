1719c214e55ecd1c5a46904c59f12fa3266db226
Gauravjeet Singh <30665291+Gauravjeet123@users.noreply.github.com>
2017-08-06 15:48:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,14 @@
 console.log('Loaded!');
 // change the text of main-text div
- var element = document.getElementById('main-text').innerHTML = 'NEW VALUE';
+ var element = document.getElementById('main-text');//.innerHTML = 'NEW VALUE';
 
-//element.innerHTML ='New value';
+element.innerHTML ='New value';
 
 // Move the image
 
-//var img = document.getElementById('madi');
-//img.onclick = function(){
+var img = document.getElementById('madi');
+img.onclick = function(){
   
   // changing the CSS from Java Script
-  //img.style.marginLeft = '100px';
-// };
+  img.style.marginLeft = '100px';
+ };
