b40405e6fdd7f53165f76a2661e602818ae949f8
Gauravjeet Singh <30665291+Gauravjeet123@users.noreply.github.com>
2017-08-07 13:52:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,31 @@
  //counter code
  
  var button = document.getElementById('counter');
- var counter = 0;
+// var counter = 0;
  
  button.onclick = function ()
  {
-     //Make a request to the counter endpoint
-     
+     // create a request object
+     var request = new XMLHttpRequest();
      
      // capture the response and store it in a variable
-     
-     
+     request.onreadystatechange = function (){
+         if (request.readyState === XMLHttpRequest.DONE){
+             // Take some action 
+             if(requst.status ===200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+             }
+         }
+         // not done anything
+     };
+        //Make a request to the counter endpoint
+     request.open('GET','gauravjeetchhabra.imad.hasura-app.io/counter',true);
+     request.send(null);
      
      //Render the variable in the correct span
      
-     counter=counter+1;
-     var span = document.getElementById('count');
-     span.innerHTML = counter.toString();
+     //counter=counter+1;
+     
 };
