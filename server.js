396aef34a4e5549cc4897f7e422c35374e926816
Gauravjeet Singh <30665291+Gauravjeet123@users.noreply.github.com>
2017-08-17 16:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,10 +94,10 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool = new Pool(config);
-app.get('/test-db', function(req,res){
+app.get('/test-db', function(req, res){
     //make a select request
     //return a response with the result
-    pool.query('Select * FROM test', function(err, results){
+    pool.query('SELECT * FROM test', function(err, results){
        if(err){
            res.status(500).send(err.toString());
        } else {
